699a93bb19442bce608fdedd464a9674ece002ab
Brad Anderson <brad.anderson87@gmail.com>
2012-05-22 11:07:33 -0500

    Didn't like seeing 2 files and 2 foreach loops

--- development/include/files.php
+++ development/include/files.php
@@ -6,10 +6,20 @@ Desc: Outputs all of the JavaScript files in js -> engine so the engine can be
 properly created without manual HTML coding.
 */
 
-// Retrive all functions
-include 'functions.php';
+//// Retrive all functions
+//include 'functions.php';
+//
+//// Display files
+//$files = get_files('js/engine','.js');
+//echo list_files($files);
 
-// Display files
-$files = get_files('js/engine','.js');
-echo list_files($files);
+
+// Cycle through js folder and echo out script tags
+foreach (scandir('js/engine') as $file) {
+    if (strpos($file, '.js')) {
+        echo '<script type="text/javascript" src="js/engine/' . $file . '"></script>';
+    }
+}
+
+
 ?>
